499ec690ae1d988dc10d27adb306ae519194850d
davincecode <hyperscript.ca@gmail.com>
2022-03-27 07:21:28 -0600

    Removed dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
 require("dotenv").config()
 
 const express = require("express")
-
 const app = express()
+const cors = require("cors")
+const sgMail = require("@sendgrid/mail")
 const port = process.env.PORT || 5000
 
-const cors = require("cors")
 app.use(cors())
-
-const bodyParser = require("body-parser")
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-const sgMail = require("@sendgrid/mail")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST")
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Expose-Headers", "x-auth-token")
   next()
 })
 
